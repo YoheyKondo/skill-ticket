b77fe50449e31144a42b7565adefd4d7c5f7fe34
batako <hatabata.5296@gmail.com>
2020-10-24 12:31:43 +0900

    サイズ感修正

--- htdocs/js/slick.js
+++ htdocs/js/slick.js
@@ -4,13 +4,23 @@ function slick(){
 		dots: true,
 	    infinite: true,
     	slidesToShow: 2,
-    	slidesToScroll: 1
-	});
+    	slidesToScroll: 1,
+    	centerMode:true,
+    	centerPadding:'0px'
+    });
 
 	$('.c_slider').slick({
 		dots: false,
     	slidesToShow: 2,
-    	slidesToScroll: 1
+    	slidesToScroll: 1,
+
+	});
+
+	$('.p_slider').slick({
+		dots: false,
+    	slidesToShow: 2,
+    	slidesToScroll: 1,
+
 	});
 
 	$('.r_slider').slick({
