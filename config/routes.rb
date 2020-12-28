99a47a30d43a2b217942da2a01bf59a11daffcb7
k-yoshi001 <cozio1126@gmail.com>
2020-12-28 08:49:36 +0000

    user edit

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   get 'privacypolicy', to: 'privacypolicy#index'
   get 'steachers', to: 'steachers#index'
   get 'myclass', to: 'myclasses#index'
-  devise_for :users
+  devise_for :users do
+    get '/users', to: redirect("/users/sign_up")
+  end
+  
   get 'search', to: 'searches#index'
   get 'result', to: 'searches#search'
   get 'about', to: 'abouts#index'
